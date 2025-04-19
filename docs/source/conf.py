a1cab3db930ce5e7cbb9ef802d7401fdcccee137
Jona Volohonsky <jona@Mac.fritz.box>
2025-04-19 15:09:51 +0200

    Rebuild docs with Furo theme, remove unsupported RTD options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,25 +19,24 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',     # Extracts docs from Python docstrings
-    'sphinx.ext.napoleon',    # Supports Google/NumPy style docstrings
-    'sphinx.ext.viewcode',    # Adds links to your Python source code
-    'sphinx.ext.autosummary', # Optional: creates summary tables
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx_rtd_theme',
 ]
-autosummary_generate = True  # Enables autosummary tables to be built
+
+autosummary_generate = True
+
 
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
+html_theme = "furo"
 html_theme_options = {
     'collapse_navigation': False,
     'navigation_depth': 4,
     'titles_only': False,
 }
-
 html_static_path = ['_static']
